2143d57352e09f8f90e3e333be969d9872640c0a
Farhan Rahim <farhanrahim160@gmail.com>
2025-11-21 06:40:18 +0000

    Container Config Done

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,14 @@
+const accountID= 232324
+let acountEmail="farhan@gmailcom"
+var accountPassword="23232"
+accountCity= "Islamabad"
+let accountState
+
+
+//accountID=3223
+//console.log(accountID);
+console.table([accountID,accountPassword,acountEmail,accountCity,accountState])
+
+/*
+prefer not to use var
+*/
---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,22 @@
+"Use Strict"; //treat as all code as newer version
+//alert(3 +3 ) we are using node.js not browser
+
+let name= "Farhan"
+let age= 23
+let isLoggedIn = false
+
+console.table([name,age,isLoggedIn])
+
+
+//number 2 to power 53
+//bightin
+//string
+//boolean
+//undefined
+//null standard value
+//symbol =>uniqueness
+
+//Objects
+console.log(typeof "212");
+console.log(typeof 23);
+console.log(typeof undefined);
