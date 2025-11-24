b96bb0baecd36ae9ce8f0ec412437274fc6d878d
Farhan Rahim <farhanrahim160@gmail.com>
2025-11-24 07:26:52 +0000

    Strings

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,33 @@
+const name="Farhan "
+const repoCount = 50
+
+// console.log(name + repoCount + " value");
+
+console.log(`My name is ${name.toUpperCase()} and my repo Count is ${repoCount}`);
+
+const gameName= new String('FARHAN')
+
+// console.log(gameName[1]);
+// console.log(gameName.length);
+// // console.log(gameName.__proto__);
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('n'));
+// console.log(`The lower case of the name is ${gameName.toLocaleLowerCase()}`);
+// console.log(`The upper case of the name is ${gameName.toUpperCase()}`);
+// console.log(`The lenght is the name is ${gameName.length}`);
+
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.substring(-8, 5)
+// console.log(anotherString);
+
+const newStringOne = "    Farhan      "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://farhanrahim.com/farhan&&20rahim"
+ console.log(url.replace ('&&20', '--'));
+ 
