fd8bc520398ac2e502441670b5ed4a399bd7d8a0
Farhan Rahim <farhanrahim160@gmail.com>
2025-11-24 06:06:41 +0000

    datatypes summary

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,17 @@
+// primitive data types
+//string, number, boolean, null, undefined, symbols,bigint
+
+//non premetive data types / reference
+//Array,objects, Functions
+
+const students=["farhan", "ali", "qurban", "alam"];
+
+let myObj={
+    name:"Farhan",
+   age :22
+}
+console.log(myObj);
+
+const myFunction= function(){
+    console.log("Hello World");
+}
