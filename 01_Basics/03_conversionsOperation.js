7c11936b43a91cd83d401348d78763393878f3ef
Farhan Rahim <farhanrahim160@gmail.com>
2025-11-24 05:06:43 +0000

    Conversion of data

---
+++ 01_Basics/03_conversionsOperation.js
@@ -0,0 +1,15 @@
+// let score = "223absd"
+
+// console.log(typeof score)
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let someNumber=33
+let stringNumber= String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
