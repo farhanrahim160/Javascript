23bbb2353dbb43fde0c40ceeccbe1528253a3fd3
Farhan Rahim <farhanrahim160@gmail.com>
2025-11-24 06:07:22 +0000

    conversions operation

--- 01_Basics/03_conversionsOperation.js
+++ 01_Basics/03_conversionsOperation.js
@@ -10,6 +10,17 @@
 let someNumber=33
 let stringNumber= String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+
+//Operations............................
+let value = 3
+let negvalue= -value
+// console.log(negvalue);
+
+let str1 ="Hello "
+let str2= "Farhan"
+let str3 =str1 + str2
+
+console.log(str3);
