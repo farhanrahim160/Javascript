0900ed011e5e739ccd6eae6e500a2d7d06e7d4aa
Farhan Rahim <farhanrahim160@gmail.com>
2025-11-24 06:30:41 +0000

    stack and heep

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -10,10 +10,35 @@ let myObj={
     name:"Farhan",
    age :22
 }
-console.log(myObj);
+// console.log(myObj);
 
 const myFunction= function(){
     console.log("Hello World")
 };
+
+//Stack and Heap
+
+let myYoutube="Farhan"
+
+let anotherName=myYoutube
+
+anotherName="Rahim"
+
+console.log(anotherName);
+console.log(myYoutube);
+
+let userOne = {
+    email: "farhan@gmail.com",
+    id: 3131
+}
+
+let  userTwo =userOne;
+
+userTwo.email= "Rahim@gmail.com"
+
+console.log(userTwo.email)
+console.log(userOne.email);
+
+
 
 
