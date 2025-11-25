4b35c279cf437730543b50ac9159b59366909994
Farhan Rahim <farhanrahim160@gmail.com>
2025-11-25 05:51:49 +0000

    Array

--- 02_Basics/01_Arrays.js
+++ 02_Basics/01_Arrays.js
@@ -1,5 +1,29 @@
-//Array
+// //Array
 
-const Arr =[0,2,2,2,2,1,4,4]
+const myArr =[0,2,2,2,2,1,4,4]
+// console.log(Arr[3]);
 
-console.log(Arr);
+// const myArr=["farhan", "ali", "alam"]
+// console.log(myArr[3]);
+// console.log(myArr);
+
+// //Array Method
+// myArr.push(5)
+// myArr.push(44)
+// myArr.push("qurban")
+// myArr.pop()
+// myArr.unshift(22)
+// myArr.shift()
+// myArr.shift()
+// myArr.shift()
+// myArr.shift()
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+console.log("A". myn1);
+
