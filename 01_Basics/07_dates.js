33dcf0196fe4cea51687080249b20addecd40e4b
Farhan Rahim <farhanrahim160@gmail.com>
2025-11-25 05:01:06 +0000

    dates

---
+++ 01_Basics/07_dates.js
@@ -0,0 +1,8 @@
+//Dates
+// let myDate =new Date()
+// console.log(myDate.toString()) ;
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date(2033, 5, 4,5,6)
+console.log(myCreatedDate.toDateString());
+
